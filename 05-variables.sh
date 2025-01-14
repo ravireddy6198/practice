3d38d4dbdb460fdf2033789be026a0974687f057
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-14 15:23:45 +0530

    ansible

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,10 +2,10 @@
   hosts: local
   connection: local
   vars:
-    course: "Devops with AWS"
+    course: Devops with AWS
     duration: 120hrs
     trainer: "Ravi Prakash"
   tasks:
   - name: print the variable
     ansible.builtin.debug:
-        msg: course is {{course}}, Duration is {{duration}}, trainer is {{trainer}}
+        msg: "course is {{course}}, Duration is {{duration}}, trainer is {{trainer}}"
